2b7de7e749c0d6315cc07835a513e8a8ebbc8fdb
Luiz Otávio Calado <64568271+ocalado@users.noreply.github.com>
2022-04-12 10:29:47 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+const botaoSortear = document.querySelector("div.box button")
+var nomeSorteado = document.querySelector("div.box h1")
+var roleta = ['André Pego','Dalila','Luiz Carlos','Loren','Lara Aniceto','Lara Grobério', 'Thalmo','Eliz','Borgo','Willian','Roberta','Mila','Stéfani','Dayse','Manu','Evilasio','Maria Luiza','Celeste','Duque','Ricieri','Vitor','Verônica','Kézia','Luiz Otávio Calado','Henrique Henning', 'Amanda Schaffert','Kamilla', 'Alessandro', 'Priscila','Ana Beatriz','Lucas Oliveira','Alê Prandi','Alê Mariani', 'Alessandro Abreu','Luiz Roberto','Lilian']
+
+
+botaoSortear.addEventListener("click", () => {
+    nomeSorteado.innerHTML = "O danzarino sorteado é " + roleta[Math.floor(Math.random() * roleta.length)]
+}) 
+
