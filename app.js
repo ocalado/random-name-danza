937329fa0a4b353fbec8ba017c47f5cb7d9eafcf
Luiz Otávio Calado <64568271+ocalado@users.noreply.github.com>
2022-07-13 17:20:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const botaoSortear = document.querySelector("div.box button")
 var nomeSorteado = document.querySelector("div.box h1")
-var roleta = ['André Pego','Dalila','Luiz Carlos','Loren','Lara Aniceto','Lara Grobério', 'Thalmo','Eliz','Borgo','Willian','Roberta','Mila','Stéfani','Dayse','Evilasio','Maria Luiza','Celeste','Duque','Ricieri','Vitor','Verônica','Kézia','Luiz Otávio Calado','Henrique Henning', 'Amanda Schaffert','Kamilla', 'Alessandro', 'Priscila','Lucas Oliveira','Luana Galvani','Alê Mariani', 'Alessandro Abreu','Luiz Roberto','Lilian', 'Rayza']
+var roleta = ['André Pego','Dalila','Luiz Carlos','Loren','Lara Aniceto','Lara Grobério','Tatiana Negris', 'Thalmo','Eliz','Borgo','Willian','Roberta','Mila','Dayse','Evilasio','Maria Luiza','Celeste','Duque','Ricieri','Verônica','Kézia','Luiz Otávio Calado','Henrique Henning', 'Amanda Schaffert','Kamilla', 'Alessandro', 'Priscila','Lucas Oliveira','Luana Galvani','Alê Mariani', 'Alessandro Abreu','Luiz Roberto','Lilian', 'Rayza']
 
 
 botaoSortear.addEventListener("click", () => {
